6347ac6218dfb2c7c83b93976d0d47da0a1857d7
nourhantaha1 <norh6142@gmail.com>
2021-09-17 17:27:14 +0200

    codes.

--- app/src/main/java/com/example/emergency/ui/MainActivity3.kt
+++ app/src/main/java/com/example/emergency/ui/MainActivity3.kt
@@ -7,40 +7,12 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.example.emergency.R
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity3 : AppCompatActivity(), View.OnClickListener {
-    lateinit var button: Button
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
-        button = findViewById(R.id.Button3)
-        button.setOnClickListener(this)
-
-
-
-
-
-
-
-    }
-
-
-    override fun onClick(v: View?) {
-        when(v?.id){
-            R.id.Button3->{
-                Toast.makeText(this, " GPS", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this,MainActivity4::class.java)
-                startActivity(intent)
-            }
-
-        }
-
-    }
-
-
-
-    /*fun onclickSignup(view: android.view.View) {
-        Toast.makeText(this, " sign up button clilked", Toast.LENGTH_SHORT).show()}*/
 
 }
+
